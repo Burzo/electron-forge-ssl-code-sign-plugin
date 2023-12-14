2fdca9f223fc307e801e8a804a9a0d679091ba8d
Miha Drofenik <miha.drofenik92@gmail.com>
2023-12-14 23:52:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# @burzo/electron-forge-ssl-code-sign-plugin
+
+Due to changes mentioned in the [electron-forge documentation](https://www.electronforge.io/guides/code-signing/code-signing-windows), the previous method of using certificates provided by [SSL.com](https://www.ssl.com/) is no longer viable. This is where our plugin comes into play.
+
+This plugin ensures that once the `@electron-forge/maker-squirrel` completes its make process, it signs the generated `exe` and `nupkg` files using the [eSigner CodeSignTool](https://www.ssl.com/guide/esigner-codesigntool-command-guide/) provided by SSL.com.
+
+Please note that `msi` files are currently not supported for signing.
+
+## Prerequisites
+
+First, you need to download the CodeSignTool suitable for your OS:
+
+- [Download for Windows](https://www.ssl.com/download/codesigntool-for-windows/)
+- [Download for Linux & macOS](https://www.ssl.com/download/codesigntool-for-linux-and-macos/)
+
+## Installation
+
+```
+npm i --save-dev @burzo/electron-forge-ssl-code-sign-plugin
+```
+
+or
+
+```
+yarn add --dev @burzo/electron-forge-ssl-code-sign-plugin
+```
+
+## Configuration
+
+The plugin accepts the following configuration variables:
+
+- `userName`: Typically, this is the email set on [express.esigner.com](https://express.esigner.com/esign) or [app.esigner.com](https://app.esigner.com/).
+- `password`: The password associated with the above `userName`.
+- `credentialId`: The eSigner credential ID found under the certificate's signing credentials on [secure.ssl.com](https://secure.ssl.com/login).
+- `signToolPath`: The **absolute** path to the CodeSignTool you downloaded from SSL.com.
+- `userTotp` (optional): The secret key generated when creating the QR code on [secure.ssl.com](https://secure.ssl.com/login). For more information, see [here](https://www.ssl.com/how-to/automate-esigner-ev-code-signing/).
+
+If you don't pass in `userTotp`, then `electron-forge` will pause the process when signing the app and wait for you to input the code via the OTP you set up on [secure.ssl.com](https://secure.ssl.com/login).
+
+Include the plugin in your Forge config as follows:
+
+```json
+    ...,
+    "plugins": [
+		{
+			name: "@burzo/electron-forge-ssl-code-sign-plugin",
+			config: {
+				userName: "some@email.com",
+				password: "mypass",
+				credentialId: "credential-id",
+				userTotp: "secret-key",
+				signToolPath: "C:/apps/my-electron-forge-app/CodeSignTool-v1.3.0-windows/CodeSignTool",
+			},
+		},
+    ],
+    ...,
+```
+
+## Contribution
+
+If anyone would like to add msi signing support, feel free to submit a PR :)
---
+++ index.ts
@@ -0,0 +1,173 @@
+import { name } from "./package.json"
+import PluginBase from "@electron-forge/plugin-base"
+import {
+	ForgeMakeResult,
+	ForgeMultiHookMap,
+	IForgeResolvableMaker,
+	ResolvedForgeConfig,
+} from "@electron-forge/shared-types"
+import { execSync, ExecSyncOptionsWithStringEncoding } from "child_process"
+import fs from "fs-extra"
+import path from "path"
+
+type ConfigTypes = {
+	userName: string
+	password: string
+	credentialId: string
+	userTotp: string
+	signToolPath: string
+}
+
+class ElectronForgeSslCodeSignPlugin extends PluginBase<ConfigTypes> {
+	name = name
+
+	constructor(config: ConfigTypes) {
+		super(config)
+		this.config = config
+	}
+
+	getHooks(): ForgeMultiHookMap {
+		return { postMake: [this.postMake] }
+	}
+
+	postMake = async (
+		forgeConfig: ResolvedForgeConfig,
+		makeResults: ForgeMakeResult[],
+	) => {
+		const { userName, password, credentialId, userTotp, signToolPath } =
+			this.config
+
+		return makeResults.map((data) => {
+			const { artifacts } = data
+
+			if (!userName || !password || !credentialId || !signToolPath) {
+				throw new Error(
+					`You did not provide all the required config variables to ${
+						this.name
+					}.\nCurrent values:\n${Object.keys(this.config)
+						.map((key) => `${key}  -  ${this.config[key as keyof ConfigTypes]}`)
+						.join("\n")}`,
+				)
+			}
+
+			const releasesPath = artifacts[0]
+			const squirrelMaker: IForgeResolvableMaker = forgeConfig.makers.find(
+				(maker) =>
+					(maker as IForgeResolvableMaker)?.name ===
+					"@electron-forge/maker-squirrel",
+			) as IForgeResolvableMaker
+
+			if (!squirrelMaker) {
+				throw new Error(
+					`The plugin ${this.name} can not work without @electron-forge/maker-squirrel. Remove it from the plugins array.`,
+				)
+			}
+
+			/**
+			 * The exe is located where RELEASES is.
+			 */
+			if (releasesPath.endsWith("RELEASES")) {
+				/**
+				 * We first parse the RELEASES file to get out the .nupkg file name
+				 * saved there. If setupExe wasn't changed in the squirrel maker
+				 * config, then we can use this parsed name to get the .exe file
+				 * name as well.
+				 *
+				 * If setupExe was set, then we need to use that instead to get
+				 * the correct .exe path for signing.
+				 */
+				const nupkgFileName = fs
+					.readFileSync(releasesPath, "utf8")
+					.split(" ")[1]
+
+				const nupkgFilePath = releasesPath.replace("RELEASES", nupkgFileName)
+
+				const exeName = squirrelMaker.config?.setupExe
+					? squirrelMaker.config?.setupExe
+					: nupkgFileName.replace("-full.nupkg", ".exe")
+
+				const exeFilePath = releasesPath.replace("RELEASES", exeName)
+
+				/**
+				 * The CodeSignTool calls other subfolders which means it needs
+				 * the absolute path in case you're calling it from anywhere
+				 * else other then it's root folder. This is their code:
+				 *
+				 * set code_sign_tool_path=%CODE_SIGN_TOOL_PATH%
+				 *
+				 * if defined code_sign_tool_path (
+				 * 		%code_sign_tool_path%\jdk-11.0.2\bin\java -jar %code_sign_tool_path%\jar\code_sign_tool-1.3.0.jar %*
+				 * ) else (
+				 * 		.\jdk-11.0.2\bin\java -jar .\jar\code_sign_tool-1.3.0.jar %*
+				 * )
+				 */
+				const { dir: codeSignToolFolder, name } = path.parse(signToolPath)
+				const codeSignToolFile = path.join(codeSignToolFolder, name)
+
+				const execSyncSettings = {
+					stdio: "inherit",
+					encoding: "utf8",
+					env: {
+						...process.env,
+						CODE_SIGN_TOOL_PATH: codeSignToolFolder,
+					},
+				} as ExecSyncOptionsWithStringEncoding
+
+				/**
+				 * We sign both the .exe and .nupkg files.
+				 */
+				try {
+					execSync(
+						`${codeSignToolFile} sign -input_file_path="${exeFilePath}" -override="true" -credential_id="${credentialId}" -username="${userName}" -password="${password}" ${
+							userTotp ? `-totp_secret="${userTotp}"` : ""
+						}`,
+						execSyncSettings,
+					)
+
+					/**
+					 * To sign the .nupkg files we also need to make sure the CodeSignTool
+					 * has the following config property set - TSA_LEGACY_URL=http://ts.ssl.com/legacy
+					 * as per https://www.ssl.com/how-to/code-signing-nuget-packages-with-esigner-codesigntool/.
+					 */
+					const pathtoCodeSignToolConf = path.join(
+						codeSignToolFolder,
+						"conf",
+						"code_sign_tool.properties",
+					)
+					const codeSignToolConfig = fs.readFileSync(
+						pathtoCodeSignToolConf,
+						"utf8",
+					)
+
+					if (
+						!codeSignToolConfig.includes(
+							"TSA_LEGACY_URL=http://ts.ssl.com/legacy",
+						)
+					) {
+						fs.appendFileSync(
+							pathtoCodeSignToolConf,
+							`\nTSA_LEGACY_URL=http://ts.ssl.com/legacy`,
+						)
+					}
+
+					execSync(
+						`${codeSignToolFile} sign -input_file_path="${nupkgFilePath}" -override="true" -credential_id="${credentialId}" -username="${userName}" -password="${password}" ${
+							userTotp ? `-totp_secret="${userTotp}"` : ""
+						}`,
+						execSyncSettings,
+					)
+				} catch (e) {
+					if (e instanceof Error) {
+						throw new Error(e.message)
+					} else {
+						throw e
+					}
+				}
+			}
+
+			return data
+		})
+	}
+}
+
+module.exports = ElectronForgeSslCodeSignPlugin
